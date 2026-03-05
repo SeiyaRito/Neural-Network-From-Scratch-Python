6bfefea31358b00cc709f99f6a2aa505a85262c7
SeiyaRito <164810882+SeiyaRito@users.noreply.github.com>
2026-03-04 20:40:30 -0600

    Delete Layer.py

--- Layer.py
+++
@@ -1,9 +0,0 @@
-from Neuron import Neuron
-import numpy as np
-
-class Layer:
-    def __init__(self, num_neurons, num_inputs_per_neuron):
-        self.neurons = [Neuron(num_inputs_per_neuron) for _ in range(num_neurons)]
-
-    def forward(self, inputs):
-        return [neuron.activate(inputs) for neuron in self.neurons]
